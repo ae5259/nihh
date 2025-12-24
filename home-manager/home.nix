845a6cc4b5487ad8fc120a83545431d3cefee707
akmal <isakulovdev@gmail.com>
2025-12-24 10:46:34 +0500

    starship config && zoxide

--- home-manager/home.nix
+++ home-manager/home.nix
@@ -98,14 +98,12 @@
     ethtool
     pciutils # lspci
     usbutils # lsusb
+
+    zoxide # for faster navigation
   ];
 
-  # basic configuration of git, please change to your own
-  programs.git = {
-    enable = true;
-    userName = "akmal";
-    userEmail = "isakulovdev@gmail.com";
-  };
+  programs.zoxide.enable = true;
+  programs.zoxide.enableFishIntegration = true;
 
   # starship - an customizable prompt for any shell
   programs.starship = {
@@ -115,22 +113,7 @@
       add_newline = false;
       aws.disabled = true;
       gcloud.disabled = true;
-      line_break.disabled = true;
-    };
-  };
-
-  # alacritty - a cross-platform, GPU-accelerated terminal emulator
-  programs.alacritty = {
-    enable = true;
-    # custom settings
-    settings = {
-      env.TERM = "xterm-256color";
-      font = {
-        size = 12;
-        draw_bold_text_with_bright_colors = true;
-      };
-      scrolling.multiplier = 5;
-      selection.save_to_clipboard = true;
+      # line_break.disabled = true;
     };
   };
 
@@ -141,13 +124,6 @@
     bashrcExtra = ''
       export PATH="$PATH:$HOME/bin:$HOME/.local/bin:$HOME/go/bin"
     '';
-
-    # set some aliases, feel free to add more or remove some
-    shellAliases = {
-      k = "kubectl";
-      urldecode = "python3 -c 'import sys, urllib.parse as ul; print(ul.unquote_plus(sys.stdin.read()))'";
-      urlencode = "python3 -c 'import sys, urllib.parse as ul; print(ul.quote_plus(sys.stdin.read()))'";
-    };
   };
 
   # This value determines the home Manager release that your
