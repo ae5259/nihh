a15e05167cc927e80b7b24a5a40928c571876240
akmal <isakulovdev@gmail.com>
2025-12-25 14:15:01 +0500

    keyboard config

--- nixos/configuration.nix
+++ nixos/configuration.nix
@@ -157,6 +157,20 @@
     '';
   };
 
+  services.kmonad = {
+    enable = true;
+    keyboards.builtin = {
+      device = "/dev/input/by-path/platform-i8042-serio-0-event-kbd";
+      config = builtins.readFile ./keyboard.kbd;
+
+      defcfg = {
+        enable = true;
+        fallthrough = true;
+        allowCommands = true;
+      };
+    };
+  };
+
   # Some programs need SUID wrappers, can be configured further or are
   # started in user sessions.
   # programs.mtr.enable = true;
