1f3ceb356126db45782e84750c407251dd2e323f
akmal <isakulovdev@gmail.com>
2026-02-05 11:05:17 +0500

    feat(ghostty): useful keybindings for navigation

--- modules/ghostty.nix
+++ modules/ghostty.nix
@@ -7,6 +7,16 @@
     settings = {
       font-family = "Iosevka";
       # font-family = "SF Mono";
+
+      keybind = [
+        "alt+h=goto_split:left"
+        "alt+l=goto_split:right"
+
+        "alt+j=previous_tab"
+        "alt+k=next_tab"
+
+        "alt+g=toggle_window_decorations"
+      ];
     };
   };
 }
