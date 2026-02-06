2d24f655bca1e051e8566126b2ecbfee04909d74
akmal <isakulovdev@gmail.com>
2026-02-06 08:53:50 +0500

    feat(ghostty): extra keybinds for splitting tabs

--- modules/ghostty.nix
+++ modules/ghostty.nix
@@ -8,13 +8,26 @@
       font-family = "Iosevka";
       # font-family = "SF Mono";
 
+      gtk-titlebar = false;
+
       keybind = [
+        # Switching in-tab splits
         "alt+h=goto_split:left"
         "alt+l=goto_split:right"
+        "alt+j=goto_split:down"
+        "alt+k=goto_split:up"
 
-        "alt+j=previous_tab"
-        "alt+k=next_tab"
+        # Splitting tabs
+        "super+h=new_split:left"
+        "super+l=new_split:right"
+        "super+j=new_split:down"
+        "super+k=new_split:up"
 
+        # Switching tabs
+        "shift+alt+j=previous_tab"
+        "shift+alt+k=next_tab"
+
+        # Remove the huge window panel on top
         "alt+g=toggle_window_decorations"
       ];
     };
