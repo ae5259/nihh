c738ea6bc2ba5fb2fc5d721fc02346158eedb197
akmal <isakulovdev@gmail.com>
2026-08-09 14:18:41 +0500

    forgot disko lol

--- flake.nix
+++ flake.nix
@@ -45,6 +45,11 @@
       inputs.nixpkgs.follows = "nixpkgs";
     };
 
+    disko = {
+      url = "github:nix-community/disko";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+
     zen-browser = {
       url = "github:0xc000022070/zen-browser-flake";
       inputs = {
